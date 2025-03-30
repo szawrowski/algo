de2bb1e571eafeda645624aaed0faeff8439db14
szawrowski <alexander.szawrowski@gmail.com>
2025-03-30 23:55:35 +0300

    feat: Add swap extended method to Array

---
+++ src/extensions/array-extensions.d.ts
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Type definitions for extended array methods.
+ * @author szawrowski
+ * @license MIT
+ */
+
+declare global {
+  interface Array<T> {
+    /**
+     * Swaps two elements in an array at the specified indices.
+     *
+     * @template T The type of elements in the array.
+     * @param i1 The index of the first element to swap.
+     * @param i2 The index of the second element to swap.
+     * @throws {Error} If either index is out of bounds.
+     * @returns The modified array with the elements swapped.
+     */
+    swap(i1: number, i2: number): T[];
+  }
+}
+
+export {};
---
+++ src/extensions/array-extensions.ts
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Extensions for standard array methods.
+ * @author szawrowski
+ * @license MIT
+ */
+
+import '@algo/extensions/array-extensions.d.ts';
+
+Array.prototype.swap = function <T>(i1: number, i2: number): T[] {
+  if (i1 < 0 || i1 >= this.length || i2 < 0 || i2 >= this.length) {
+    throw new Error('Index out of bounds');
+  }
+  [this[i1], this[i2]] = [this[i2], this[i1]];
+  return this;
+};
